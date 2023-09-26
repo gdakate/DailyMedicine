242bbbdeebb8463a786264415e489bd0c9ddf925
gdakate <gdakate1215@gmail.com>
2023-09-26 21:58:07 +0900

    Initial commit

---
+++ src/main/java/com/gradproject/dailymedicine/DailyMedicineApplication.java
@@ -0,0 +1,13 @@
+package com.gradproject.dailymedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyMedicineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyMedicineApplication.class, args);
+    }
+
+}
